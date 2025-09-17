b6ef4d25f56036041f540d490e637837f760281a
fatimazafarrizvi <fatimarz1215@gmail.comm>
2025-09-17 08:37:07 +0530

    hi2

--- Routes/admin_routes.js
+++ Routes/admin_routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const admin = require("../api/adminapi")
 
-router.get("/admin_all", admin.adminfind) //login
+router.post("/admin_all", admin.adminfind) //login
 router.post("/admin_insert", admin.insert_admin) //sign_up
 
 module.exports = router
