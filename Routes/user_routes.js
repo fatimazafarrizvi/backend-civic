ff67a1dc75e84c16e935678d60802c7b46d9a7a3
fatimazafarrizvi <fatimarz1215@gmail.comm>
2025-09-17 08:40:44 +0530

    hi3

--- Routes/user_routes.js
+++ Routes/user_routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const user = require("../api/usersapi")
 
-router.get("/user_all", user.userfind) //login
+router.post("/user_all", user.userfind) //login
 router.post("/user_insert", user.insert_user) //sign_up
 
 module.exports = router
